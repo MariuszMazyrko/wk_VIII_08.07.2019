67b5917efb7c79fac3a460e82addf592ce615bef
Mariusz Mazyrko <m-mazyrko@gmail.com>
2019-08-23 10:18:15 +0200

    mySimpleEditor_DONE

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,54 @@
-"use strict";
+const text__box = document.querySelector('.text__box--js');
+const load = document.querySelector('.button__load--js');
+const save = document.querySelector('.button__save--js');
 
-import moment from 'moment';
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+console.log(load);
+console.log(save);
+console.log(text__box);
+
+save.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(text__box.value)
+    localStorage.setItem('text__box--js', text__box.value);
+})
+
+load.addEventListener('click', (e) => {
+    e.preventDefault();
+    text__box.value = localStorage.getItem('text__box--js');
+})
+
+
+
+// focusInput.addEventListener('keyup', (e) => {
+//     console.log(e.value);
+// });
+
+// if (localStorage.getItem('focusInput').length > 0) {
+//     focusInput.value = localStorage.getItem('focusInput');
+// }
+
+// focusInput.addEventListener('keyup', (e) => {
+//     localStorage.setItem('focusInput', e.target.value);
+// });
+
+const person = {
+    name: 'Mariusz',
+    age: 39
+}
+
+console.log(person);
+const jsonPerson = JSON.stringify(person);
+console.log(jsonPerson);
+
+localStorage.setItem('person', jsonPerson);
+
+const getItemPerson = localStorage.getItem('person');
+
+const newPerson = JSON.parse(getItemPerson);
+console.log(newPerson);
+
+
+localStorage.setItem('imie', 'Mariusz');
+localStorage.setItem('nazwisko', 'Mazyrko');
